bf28ae306b66498ffddbd3888012562d59fe3214
wenzhijie <wenzhijie@huibur.com>
2020-02-12 23:07:02 +0800

    add:global token

--- src/main/java/com/huibur/furion/swagger/utils/SwaggerUtil.java
+++ src/main/java/com/huibur/furion/swagger/utils/SwaggerUtil.java
@@ -2,12 +2,17 @@ package com.huibur.furion.swagger.utils;
 
 import com.huibur.furion.common.io.PropertiesUtils;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class SwaggerUtil {
@@ -18,11 +23,12 @@ public class SwaggerUtil {
         String applicationContext = PropertiesUtils.getInstance().getProperty("server.servlet.context-path", "/Furion");
         return new ApiInfoBuilder()
                 .title(applicationContext.substring(1) +" RESTful APIs")
-                .version("1.0")
+                .version("1.0.0")
                 .description("API 描述")
                 .contact(new Contact("HuiBur", "", ""))
                 .build();
     }
+
 
     public static Docket initDocket() {
         return new Docket(DocumentationType.SWAGGER_2)
@@ -30,6 +36,47 @@ public class SwaggerUtil {
                 .select()   // 选择那些路径和api会生成document
                 .apis(RequestHandlerSelectors.basePackage("com.huibur"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .securitySchemes(securitySchemes())
+                .securityContexts(securityContexts());
     }
+
+    private static List<Parameter> setHeaderToken() {
+        ParameterBuilder tokenPar = new ParameterBuilder();
+        List<Parameter> pars = new ArrayList<>();
+        tokenPar.name("Authorization")
+                .description("token")
+                .modelRef(new ModelRef("string"))
+                .parameterType("header")
+                .required(false)
+                .build();
+        pars.add(tokenPar.build());
+        return pars;
+    }
+
+    private static List<ApiKey> securitySchemes() {
+        List<ApiKey> apiKeyList= new ArrayList();
+        apiKeyList.add(new ApiKey("Authorization", "Token", "header"));
+        return apiKeyList;
+    }
+
+    private static List<SecurityContext> securityContexts() {
+        List<SecurityContext> securityContexts=new ArrayList<>();
+        securityContexts.add(
+                SecurityContext.builder()
+                        .securityReferences(defaultAuth())
+                        .forPaths(PathSelectors.regex("^(?!login).*$"))
+                        .build());
+        return securityContexts;
+    }
+
+    private static  List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        List<SecurityReference> securityReferences=new ArrayList<>();
+        securityReferences.add(new SecurityReference("Authorization", authorizationScopes));
+        return securityReferences;
+    }
+
 }
